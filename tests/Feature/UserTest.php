086357419a0c7bfe2fd08a1ee612197699d75e05
Luiz Henrique Alexandretti <luiizhenrique1@hotmail.com>
2018-01-08 22:45:21 -0200

    Unit Test
    Adição de Unit Test.

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\User;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $user = new User(['email'=>'luiz@email.com']);
+      	$this->assertEquals('luiz@email.com', $user->email);    }
+}
